a986ba1e70cabe1884ccb2fe022aa4032d100baf
muharromi42 <muharromi42@gmail.com>
2024-10-01 22:47:16 +0700

    add: single post article and slug

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,16 +16,18 @@ Route::get('/about', function () {
 
 Route::get('/posts', function () {
     return view('posts', [
-        'title' => 'posts',
+        'title' => 'blog',
         'posts' => [
             [
                 'id' => 1,
+                'slug' => 'judul-1',
                 'title' => 'judul 1',
                 'author' => 'muharromi',
                 'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus porro architecto repellendus quas cumque blanditiis vero odio similique itaque ab minima laborum neque nisi ducimus, excepturi tempora sunt amet placeat!'
             ],
             [
                 'id' => 2,
+                'slug' => 'judul-2',
                 'title' => 'judul 2',
                 'author' => 'Ardhana',
                 'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil quasi est maiores amet accusantium nemo tenetur numquam facilis quia illo laboriosam quae qui, excepturi, veniam quis officiis earum harum pariatur!'
@@ -33,8 +36,29 @@ Route::get('/posts', function () {
     ]);
 });
 
-Route::get('/posts/{id}', function ($id) {
-    dd($id);
+Route::get('/posts/{id}', function ($slug) {
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'judul-1',
+            'title' => 'judul 1',
+            'author' => 'muharromi',
+            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus porro architecto repellendus quas cumque blanditiis vero odio similique itaque ab minima laborum neque nisi ducimus, excepturi tempora sunt amet placeat!'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-2',
+            'title' => 'judul 2',
+            'author' => 'Ardhana',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil quasi est maiores amet accusantium nemo tenetur numquam facilis quia illo laboriosam quae qui, excepturi, veniam quis officiis earum harum pariatur!'
+        ],
+    ];
+
+    $post = Arr::first($posts, function ($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
