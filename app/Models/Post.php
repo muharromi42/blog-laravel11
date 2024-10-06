cd1eb39e97eb74570dd15d27f85dc0c1e54f8fb7
muharromi42 <muharromi42@gmail.com>
2024-10-06 23:48:00 +0700

    fix: model fillable

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,4 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Post extends Model {}
+class Post extends Model
+{
+    protected $fillable = ['title', 'author', 'slug', 'body'];
+}
