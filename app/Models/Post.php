e1e9248c37ae80211f6e6ba9c60e03803febfa50
muharromi42 <muharromi42@gmail.com>
2024-10-07 22:51:25 +0700

    fix: models and carbon

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Arr;
+
 
 class Post extends Model
 {
+    use HasFactory;
     protected $fillable = ['title', 'author', 'slug', 'body'];
 }
